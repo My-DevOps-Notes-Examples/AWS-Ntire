3d6bf351e91b9a5475e22939dabdbb6683113864
nagasuribabukola <nagasuribabukola@gmail.com>
2023-12-11 18:08:15 +0530

    make the changes using length function

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ resource "aws_vpc" "ntier" {
 }
 
 resource "aws_subnet" "subnets" {
-  count = 4
-
+  count             = length(var.subnets-names)
   vpc_id            = aws_vpc.ntier.id
   cidr_block        = var.subnets-cidr[count.index]
   availability_zone = "${var.region}${var.subnets-azs[count.index]}"

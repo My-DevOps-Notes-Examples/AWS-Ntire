7657d22711fd2e81b82cdb7193fa4d72484c6ef1
nagasuribabukola <nagasuribabukola@gmail.com>
2023-12-11 17:24:01 +0530

    added subnets using string interpolation

--- inputs.tf
+++ inputs.tf
@@ -9,3 +9,23 @@ variable "ntier-vpc-range" {
   default     = "192.168.0.0/16"
   description = "VPC CIDR range"
 }
+
+variable "app1-cidr" {
+  type    = string
+  default = "192.168.0.0/24"
+}
+
+variable "app2-cidr" {
+  type    = string
+  default = "192.168.1.0/24"
+}
+
+variable "db1-cidr" {
+  type    = string
+  default = "192.168.2.0/24"
+}
+
+variable "db2-cidr" {
+  type    = string
+  default = "192.168.3.0/24"
+}
--- main.tf
+++ main.tf
@@ -5,3 +5,59 @@ resource "aws_vpc" "ntier" {
     Name = "ntier"
   }
 }
+
+resource "aws_subnet" "app1" {
+  vpc_id            = aws_vpc.ntier.id
+  cidr_block        = var.app1-cidr
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "app1"
+  }
+
+  depends_on = [
+    aws_vpc.ntier
+  ]
+}
+
+resource "aws_subnet" "app2" {
+  vpc_id            = aws_vpc.ntier.id
+  cidr_block        = var.app2-cidr
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "app2"
+  }
+
+  depends_on = [
+    aws_vpc.ntier
+  ]
+}
+
+resource "aws_subnet" "db1" {
+  vpc_id            = aws_vpc.ntier.id
+  cidr_block        = var.db1-cidr
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "db1"
+  }
+
+  depends_on = [
+    aws_vpc.ntier
+  ]
+}
+
+resource "aws_subnet" "db2" {
+  vpc_id            = aws_vpc.ntier.id
+  cidr_block        = var.db2-cidr
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "db2"
+  }
+
+  depends_on = [
+    aws_vpc.ntier
+  ]
+}
